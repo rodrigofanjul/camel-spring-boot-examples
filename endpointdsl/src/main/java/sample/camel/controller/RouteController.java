cfe8003e36d815c3b59ca44e8be0aec7a3d8da92
Rodrigo Fanjul <rodrii.fan@gmail.com>
2024-12-08 21:17:38 -0300

    ADD: Docker-compose and poc routes

---
+++ endpointdsl/src/main/java/sample/camel/controller/RouteController.java
@@ -0,0 +1,27 @@
+package sample.camel.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RouteController {
+
+    private final org.apache.camel.ProducerTemplate producerTemplate;
+
+    public RouteController(org.apache.camel.ProducerTemplate producerTemplate) {
+        this.producerTemplate = producerTemplate;
+    }
+
+    @GetMapping("/start-combined-route/{param}")
+    public String startCombinedRoute(@PathVariable int param) {
+        return producerTemplate.requestBodyAndHeader("direct:combinedApi", null, "param", param, String.class);
+    }
+
+    @GetMapping("/start-api-kafka-route/{param}")
+    public String startApiKafkaRoute(@PathVariable int param) {
+        return producerTemplate.requestBodyAndHeader("direct:apiKafka", null, "param", param, String.class);
+    }
+}
